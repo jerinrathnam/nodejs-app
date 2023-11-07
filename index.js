703dfb0fd9f8d8c325264bc0ce43c0aad1b8e584
JerinRathnam <61724686+jerinrathnam@users.noreply.github.com>
2023-11-07 21:33:06 +0530

    add healthcheck

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/health', (req, res) => {
+  res.status(200).send('Health check passed');
+});
+
 app.get('/',(req, res) => res.send('Hello! This is Jerin Rathnam'));
 app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
